17d1f133fd8ab2f7a2dd1648db4fa25b2c1e8a87
Marcelo Palmito <maisnada@gmail.com>
2022-11-23 21:19:29 -0300

    prototype

---
+++ objetos/prototype/script.js
@@ -0,0 +1,30 @@
+function Pessoa(nome, idade) {
+
+    this.nome = nome;
+    this.idade = idade;
+}
+
+Pessoa.prototype.andar = function () {
+
+    console.log('...');
+}
+
+let aluno = new Pessoa('Marcelo', 35);
+
+console.log(Pessoa.prototype)
+
+console.log(aluno);
+
+aluno.andar();
+
+let nome = String('Rosa');
+
+console.log(nome);
+
+const animais = ['Cachorro', 'Gato'];
+
+console.log(animais);
+
+console.log(animais.constructor.name);
+console.log(aluno.constructor.name);
+console.log(nome.constructor.name);
