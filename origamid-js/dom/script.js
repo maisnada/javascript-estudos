d263452d09fa13ec25375365db6ba8f36f35471a
Marcelo Moreno <marcelo.moreno@univali.br>
2025-12-23 10:46:21 -0300

    dom e projeto

---
+++ origamid-js/dom/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+console.log(window);
+
+let h1 = document.querySelector('h1');
+
+console.log(h1);
+console.log(h1.innerText);
+console.log(h1.classList);
+
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
+console.log(window.innerWidth);
